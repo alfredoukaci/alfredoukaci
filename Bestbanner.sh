0ed047e9dcc80eb175953b2683e64dd0d626fcdd
bequietandlisten <fredookaci@gmail.com>
2019-12-14 05:00:10 +0100

    Add files via upload
    simple banner for linux based systems

---
+++ Bestbanner.sh
@@ -0,0 +1,163 @@
+#!/bin/bash -i
+  #program by farid oukaci 
+  #this program helps in changing the banner for terminal emulator for linux based systems
+  #i hope you'll enjoy it 
+  # Array with expressions
+  currentDate=`date`
+  
+abouch=( "▂▂▂▂▂▂▂▂▂▂▂▂▂▃▅▇██▓▓▒▒░۩۞۩ ۩۞۩░▒▒▓▓██▇▅▃▂▂▂▂▂▂▂▂▂▂▂▂▂" "●▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬●" "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬"   "▂▂▂▂▂▂▂▃▄▄▄▅▅▆▆▇▇▇▇█▓▒░LoveYou░▒▓█▇▇▇▇▆▆▅▅▄▄▄▃▂▂▂▂▂▂▂▂" )
+downbouch=( "❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀❀" )
+RANDOM=$$$(date +%s)
+while [ 1 ]
+do 
+    selectedmagic=${abouch[$RANDOM % ${#abouch[@]} ]}
+    echo $selectedmagic
+break
+
+done 
+u="$USER"
+expressions=( "                      $u︻╦̵̵͇̿̿̿̿╤───  ဗီူဗီူ ဗီူဗီူဗီူဗီူ ဗီူဗီူ" "$u" "██░░░░░$u░░░░░██" "ι═══════ﺤ $u -═══════ι" "˙·٠•●♥ $u ♥●•٠·˙" "•♥•♥•♥•♥•♥ $u ♥•♥•♥•♥•♥•" "███████▒▒▒ 70%" ".██████████████]99%Complete..." "ס₪₪₪₪§|(Ξ≥≤≥≤≥≤ΞΞΞΞΞΞΞΞΞΞ>" "[ ▇ ▄ ▅ █ ▇ ▂ ▃ $u▁ ▄ ▅ █ ▅ ▇ ]" "$u ()==[:::::::::::::> ဗီူဗီူ ဗီူဗီူ"
+ "            cxxx{くろ}ҳ̸Ҳ̸ҳҳ̸Ҳ̸ҳҳ̸Ҳ̸ҳҳ̸Ҳ̸ҳ­ҳ̸Ҳ̸ҳҳ̸Ҳ̸ҳҳ̸Ҳ̸ҳ❥" "     $u︻╦̵̵͇̿̿̿̿╤───  ဗီူဗီူ ဗီူဗီူဗီူဗီူ ဗီူဗီူ" "𝕎𝕙𝕒𝕥'𝕤 𝕕𝕠𝕟𝕖 𝕚𝕤 𝕕𝕠𝕟𝕖" "𝕋𝕙𝕚𝕤 𝕥𝕠𝕠 𝕤𝕙𝕒𝕝𝕝 𝕡𝕒𝕤𝕤" "ℂ𝕠𝕦𝕣𝕒𝕘𝕖 𝕕𝕠𝕖𝕤𝕟'𝕥 𝕒𝕝𝕨𝕒𝕪𝕤 𝕣𝕠𝕒𝕣" "ℕ𝕠𝕥𝕙𝕚𝕟𝕘 𝕖𝕟𝕕𝕦𝕣𝕖𝕤 𝕓𝕦𝕥 𝕔𝕙𝕒𝕟𝕘𝕖" "ℕ𝕠 𝕗𝕖𝕖𝕝𝕚𝕟𝕘 𝕚𝕤 𝕗𝕚𝕟𝕒𝕝" "𝕐𝕠𝕦 𝕒𝕣𝕖 𝕪𝕠𝕦𝕣 𝕔𝕙𝕠𝕚𝕔𝕖𝕤" "𝕝𝕚𝕗𝕖 𝕚𝕤 𝕔𝕙𝕠𝕚𝕔𝕖 " "𝕣𝕖𝕒𝕔𝕙 𝕗𝕠𝕣 𝕥𝕙𝕖 𝕤𝕥𝕒𝕣𝕤" "𝕟𝕠𝕥𝕙𝕚𝕟𝕘 𝕧𝕖𝕟𝕥𝕦𝕣𝕖𝕕,𝕟𝕠𝕥𝕙𝕚𝕟𝕘 𝕘𝕒𝕚𝕟𝕖𝕕" "𝕃𝕚𝕧𝕖 𝕒𝕟𝕕 𝕝𝕖𝕥 𝕝𝕚𝕧𝕖" " 𝕃𝕚𝕗𝕖 𝕥𝕖𝕒𝕔𝕙𝕖𝕤, 𝕃𝕠𝕧𝕖 𝕣𝕖𝕧𝕖𝕒𝕝𝕤" "ℂ𝕣𝕖𝕒𝕥𝕚𝕧𝕚𝕥𝕪 𝕚𝕤 𝕝𝕚𝕜𝕖 𝕖𝕝𝕖𝕔𝕥𝕣𝕚𝕔𝕚𝕥𝕪" "ℍ𝕒𝕡𝕡𝕚𝕟𝕖𝕤𝕤 𝕕𝕖𝕡𝕖𝕟𝕕𝕤 𝕦𝕡𝕠𝕟 𝕠𝕦𝕣𝕤𝕖𝕝𝕧𝕖𝕤" "ℕ𝕠𝕥𝕙𝕚𝕟𝕘 𝕤𝕦𝕔𝕔𝕖𝕖𝕕𝕤 𝕝𝕚𝕜𝕖 𝕤𝕦𝕔𝕔𝕖𝕤𝕤" "𝕊𝕦𝕔𝕔𝕖𝕤𝕤 𝕔𝕒𝕟'𝕥 𝕓𝕖 𝕗𝕠𝕣𝕔𝕖𝕕 " "𝔹𝕖 𝕔𝕦𝕣𝕚𝕠𝕦𝕤, 𝕟𝕠𝕥 𝕛𝕦𝕕𝕘𝕞𝕖𝕟𝕥𝕒𝕝" "ℕ𝕒𝕥𝕦𝕣𝕖 𝕚𝕤 𝕞𝕪 𝕞𝕖𝕕𝕚𝕔𝕚𝕟𝕖" "𝕋𝕒𝕝𝕖𝕟𝕥 𝕨𝕠𝕣𝕜𝕤, 𝕘𝕖𝕟𝕚𝕦𝕤 𝕔𝕣𝕖𝕒𝕥𝕖𝕤" "𝔸 𝕞𝕠𝕥𝕙𝕖𝕣'𝕤 𝕝𝕠𝕧𝕖 𝕝𝕚𝕓𝕖𝕣𝕒𝕥𝕖𝕤" "  𝔸𝕚𝕞 𝕗𝕠𝕣 𝕥𝕙𝕖 𝕙𝕚𝕘𝕙𝕖𝕤𝕥 " "𝔹𝕖𝕘𝕚𝕟𝕟𝕚𝕟𝕘𝕤 𝕒𝕣𝕖 𝕒𝕝𝕨𝕒𝕪𝕤 𝕞𝕖𝕤𝕤𝕪"  "ℂ𝕠𝕦𝕣𝕒𝕘𝕖 𝕕𝕠𝕖𝕤𝕟'𝕥 𝕒𝕝𝕨𝕒𝕪𝕤 𝕣𝕠𝕒𝕣 " "𝔾𝕠𝕕 𝕕𝕠𝕖𝕤𝕟'𝕥 𝕞𝕒𝕜𝕖 𝕛𝕦𝕟𝕜"  "𝕀 𝕙𝕒𝕧𝕖 𝕒 𝕕𝕣𝕖𝕒𝕞"  "𝕀𝕥'𝕤 𝕟𝕖𝕧𝕖𝕣 𝕥𝕠𝕠 𝕝𝕒𝕥𝕖 " "ℕ𝕠 𝕡𝕒𝕚𝕟, 𝕟𝕠 𝕘𝕒𝕚𝕟" " 𝕋𝕠𝕠 𝕔𝕝𝕖𝕧𝕖𝕣 𝕚𝕤 𝕕𝕦𝕞𝕓" " 𝕙𝕒𝕧𝕖 𝕦 𝕒𝕤𝕜𝕖𝕕 𝕒𝕓𝕥 𝕪𝕠𝕦𝕣 𝕘𝕗 𝕥𝕠𝕕𝕒𝕪 !"  " 𝕕𝕠𝕟'𝕥 𝕗𝕠𝕣𝕘𝕖𝕥,𝕓𝕖 𝕒𝕟𝕠𝕟𝕪𝕞𝕠𝕦𝕤" "𝕙𝕒𝕧𝕖 𝕒 𝕘𝕠𝕠𝕕 𝕕𝕒𝕪 𝕞𝕒𝕟 !" "𝕪𝕠𝕦 𝕔𝕒𝕟 𝕔𝕣𝕒𝕔𝕜 𝕚𝕥 " "𝕝𝕚𝕟𝕦𝕩 𝕚𝕤 𝕥𝕙𝕖 𝕓𝕖𝕤𝕥 " "𝕋𝕙𝕠𝕦𝕘𝕙𝕥𝕤 𝕣𝕦𝕝𝕖 𝕥𝕙𝕖 𝕨𝕠𝕣𝕝𝕕" "𝕋𝕙𝕖 𝕢𝕦𝕚𝕖𝕥𝕖𝕣 𝕪𝕠𝕦 𝕓𝕖𝕔𝕠𝕞𝕖, 𝕥𝕙𝕖 𝕓𝕖𝕤𝕥 𝕪𝕠𝕦 𝕒𝕣𝕖 𝕒𝕓𝕝𝕖 𝕥𝕠 𝕙𝕖𝕒𝕣…" " ℕ𝕖𝕧𝕖𝕣 𝕥𝕖𝕝𝕝 𝕖𝕧𝕖𝕣𝕪𝕥𝕙𝕚𝕟𝕘 𝕪𝕠𝕦 𝕜𝕟𝕠𝕨" "𝕃𝕠𝕧𝕖 𝔽𝕠𝕣 𝔸𝕝𝕝, ℍ𝕒𝕥𝕣𝕖𝕕 𝔽𝕠𝕣 ℕ𝕠𝕟𝕖" "ℂ𝕙𝕒𝕟𝕘𝕖 𝕥𝕙𝕖 𝕨𝕠𝕣𝕝𝕕 𝕓𝕪 𝕓𝕖𝕚𝕟𝕘 𝕪𝕠𝕦𝕣𝕤𝕖𝕝𝕗" "ℕ𝕖𝕧𝕖𝕣 𝕣𝕖𝕘𝕣𝕖𝕥 𝕒𝕟𝕪𝕥𝕙𝕚𝕟𝕘 𝕥𝕙𝕒𝕥 𝕞𝕒𝕕𝕖 𝕪𝕠𝕦 𝕤𝕞𝕚𝕝𝕖" "𝔻𝕚𝕖 𝕨𝕚𝕥𝕙 𝕞𝕖𝕞𝕠𝕣𝕚𝕖𝕤, 𝕟𝕠𝕥 𝕕𝕣𝕖𝕒𝕞𝕤"   " 𝔸𝕤𝕡𝕚𝕣𝕖 𝕥𝕠 𝕚𝕟𝕤𝕡𝕚𝕣𝕖 𝕓𝕖𝕗𝕠𝕣𝕖 𝕨𝕖 𝕖𝕩𝕡𝕚𝕣𝕖" "𝕎𝕙𝕒𝕥 𝕨𝕖 𝕥𝕙𝕚𝕟𝕜, 𝕨𝕖 𝕓𝕖𝕔𝕠𝕞𝕖" "𝕐𝕖𝕤𝕥𝕖𝕣𝕕𝕒𝕪 𝕪𝕠𝕦 𝕤𝕒𝕚𝕕 𝕥𝕠𝕞𝕠𝕣𝕣𝕠𝕨. 𝕁𝕦𝕤𝕥 𝕕𝕠 𝕚𝕥" "𝕊𝕥𝕣𝕚𝕧𝕖 𝕗𝕠𝕣 𝕘𝕣𝕖𝕒𝕥𝕟𝕖𝕤𝕤" "𝕋𝕦𝕣𝕟 𝕪𝕠𝕦𝕣 𝕨𝕠𝕦𝕟𝕕𝕤 𝕚𝕟𝕥𝕠 𝕨𝕚𝕤𝕕𝕠𝕞" "⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜ 10%" "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬜ 90%" "⚫⚫⚫⚫⚫⚫⚫⚫⚫⚪⚪⚪⚪⚪⚪ 60%" "████████████▒░ 88.8%" "▁ ▂ ▃ ▄ ▅ ▆ █ 100 %" "██▓▒░░._____.░░▒▓██")
+# Seed random generator
+RANDOM=$$$(date +%s)
+
+# Loop loop loop loop loop loop ...
+while [ 1 ]
+do
+    # Get random expression...
+    selectedexpression=${expressions[$RANDOM % ${#expressions[@]} ]}
+    
+
+    # Write to Shell
+    echo $selectedexpression
+break
+
+done
+
+u="$USER"
+  
+  title="♠ $selectedexpression ♠"
+  clear
+  
+  columnas=$(tput cols)
+  y=$((($columnas-${#selectedmagic})/2))
+  x=0
+  tput clear
+  tput cup $x $y
+  WORDS=( $selectedmagic )
+for WORD in "${selectedmagic[@]}"; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+  y=$((($columnas-${#title})/2))
+  x=1
+  tput cup $x $y
+  WORDS=( $title )
+for WORD in "${title[@]}"; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+  y=$((($columnas-${#downbouch})/2))
+  x=2
+  tput cup $x $y
+  WORDS=( $downbouch )
+for WORD in "${downbouch[@]}"; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+bann=(" ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓ ██▓")
+bann1=("▓░░▒▓██▒▓░░▒▓██▒▓██▒▓██▒▓██▒▓██▒▓██▒▓██▒▓██▒▓██▒▓░░▒▓██▒")
+bann2=("▒██▒▒██▒▒██▒▒██▒▒░░▒▒██▒▒██▒▒██▒▒██▒▒░░▒▒██▒▒██▒▒██▒▒██▒")
+bann3=("░██░░██░░██░░██░░██░░██░░██░░██░░██░░██░░██░░██░░██░░██░")
+bann4=("░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ░▓  ")
+bann5=(" ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░")
+bann6=(" ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒ ░ ▒dz░")
+bann7=(" ░ $currentDate░   ░   ░   ░   ░   ░  ")
+           
+    
+    columnas=$(tput cols)
+    y=$((($columnas-${#bann})/2))
+    x=3
+    tput cup $x $y
+    WORDS=( $bann )
+    for WORD in "${bann[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    y=$((($columnas-${#bann1})/2))
+    x=4
+    tput cup $x $y
+    WORDS=( $bann1 )
+    for WORD in "${bann1[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    y=$((($columnas-${#bann2})/2))
+    x=5
+    tput cup $x $y
+    WORDS=( $bann2 )
+    for WORD in "${bann2[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    y=$((($columnas-${#bann3})/2))
+    x=6
+    tput cup $x $y
+    WORDS=( $bann3 )
+    for WORD in "${bann3[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    y=$((($columnas-${#bann4})/2))
+    x=7
+    tput cup $x $y
+    WORDS=( $bann4 )
+    for WORD in "${bann4[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    y=$((($columnas-${#bann5})/2))
+    x=8
+    tput cup $x $y
+    WORDS=( $bann5 )
+    for WORD in "${bann5[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    y=$((($columnas-${#bann6})/2))
+    x=9
+    tput cup $x $y
+    WORDS=( $bann6 )
+    for WORD in "${bann6[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    y=$((($columnas-${#bann7})/2))
+    x=10
+    tput cup $x $y
+    WORDS=( $bann7 )
+    for WORD in "${bann7[@]}" ; do
+    let "i=$RANDOM % 256"
+    echo -en "\e[38;5;${i}m$WORD \e[0m";
+    done;
+    echo ""
+    
+    
